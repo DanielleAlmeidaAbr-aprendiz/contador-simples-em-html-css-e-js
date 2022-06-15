8bdc406ef77f1e50c0f7a6a637f7f8fb0dc880db
DanielleAlmeidaAbr-aprendiz <106355030+DanielleAlmeidaOliveira@users.noreply.github.com>
2022-06-15 17:57:54 -0300

    comit inicial

---
+++ assets/js/scripts.js
@@ -0,0 +1,14 @@
+var currentNumberWarpper = document.getElementById("currentNumber")
+var currentNumber = 0;
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWarpper.innerHTML = currentNumber
+}
+var currentNumberWarpper = document.getElementById("currentNumber")
+var currentNumber = 0;
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWarpper.innerHTML = currentNumber
+}
